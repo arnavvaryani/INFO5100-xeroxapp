f1fd84ccf6449f2a239907cdb147add6a13ea76d
Neha Devarapalli <devarapalli.n@northeastern.edu>
2023-11-16 00:28:10 -0500

    formatting

--- src/TheBusiness/Supplier/Supplier.java
+++ src/TheBusiness/Supplier/Supplier.java
@@ -15,40 +15,45 @@ import TheBusiness.ProductManagement.ProductsReport;
  * @author kal bugrara
  */
 public class Supplier {
+
     String name;
     ProductCatalog productcatalog;
-   ProductsReport productsreport;
-    public Supplier(String n){
+    ProductsReport productsreport;
+
+    public Supplier(String n) {
         name = n;
         productcatalog = new ProductCatalog("software");
-        
+
     }
-    
-    public ProductsReport prepareProductsReport(){
-        
+
+    public ProductsReport prepareProductsReport() {
+
         productsreport = productcatalog.generatProductPerformanceReport();
         return productsreport;
     }
-    
-    public ArrayList<ProductSummary> getProductsAlwaysAboveTarget(){
-       
-        if(productsreport==null) productsreport = prepareProductsReport();
-       return productsreport.getProductsAlwaysAboveTarget();
-       
+
+    public ArrayList<ProductSummary> getProductsAlwaysAboveTarget() {
+
+        if (productsreport == null) {
+            productsreport = prepareProductsReport();
+        }
+        return productsreport.getProductsAlwaysAboveTarget();
+
     }
-    
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-        public ProductCatalog getProductCatalog(){
+
+    public ProductCatalog getProductCatalog() {
         return productcatalog;
     }
     //add supplier product ..
-    
+
     //update supplier product ...
     @Override
-   public String toString(){
-       return name;
-       
-   }
+    public String toString() {
+        return name;
+
+    }
 }
--- src/TheBusiness/UserAccountManagement/UserAccount.java
+++ src/TheBusiness/UserAccountManagement/UserAccount.java
@@ -12,38 +12,45 @@ import TheBusiness.Personnel.Profile;
  * @author kal bugrara
  */
 public class UserAccount {
-    
+
     Profile profile;
     String username;
     String password;
-    
-    public UserAccount (Profile profile, String un, String pw){
+
+    public UserAccount(Profile profile, String un, String pw) {
         username = un;
-         password = pw;
-         this.profile = profile;
+        password = pw;
+        this.profile = profile;
 
     }
 
-    public String getPersonId(){
+    public String getPersonId() {
         return profile.getPerson().getPersonId();
     }
 
-        public boolean isMatch(String id){
-        if(getPersonId().equals(id)) return true;
+    public boolean isMatch(String id) {
+        if (getPersonId().equals(id)) {
+            return true;
+        }
         return false;
     }
-        public boolean IsValidUser(String un, String pw){
-        
-            if (username.equalsIgnoreCase(un) && password.equals(pw)) return true;
-            else return false;
-        
+
+    public boolean IsValidUser(String un, String pw) {
+
+        if (username.equalsIgnoreCase(un) && password.equals(pw)) {
+            return true;
+        } else {
+            return false;
         }
-        public String getRole(){
-            return profile.getRole();
-        }
-        
-        public Profile getAssociatedPersonProfile(){
-            return profile;
-        }    
-        
+
+    }
+
+    public String getRole() {
+        return profile.getRole();
+    }
+
+    public Profile getAssociatedPersonProfile() {
+        return profile;
+    }
+
 }
