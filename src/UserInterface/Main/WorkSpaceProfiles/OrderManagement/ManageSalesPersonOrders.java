a91421567a4755e42d39d342bce1cd813203d6ed
Neha Devarapalli <devarapalli.n@northeastern.edu>
2023-11-19 17:28:42 -0500

    Added feature to auto-populate textfields in SalesPersonOrders

--- src/UserInterface/Main/WorkSpaceProfiles/OrderManagement/ManageSalesPersonOrders.java
+++ src/UserInterface/Main/WorkSpaceProfiles/OrderManagement/ManageSalesPersonOrders.java
@@ -52,7 +52,7 @@ public class ManageSalesPersonOrders extends javax.swing.JPanel {
             row[1] = s.getQuantity();
             row[2] = s.getCustomerProfile().getPerson().getPersonId();
             
-            ((DefaultTableModel) SupplierCatalogTable.getModel()).addRow(row);
+            ((DefaultTableModel) SalesOrderTable.getModel()).addRow(row);
         }
     }
     /**
@@ -67,12 +67,16 @@ public class ManageSalesPersonOrders extends javax.swing.JPanel {
         Back = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         jScrollPane1 = new javax.swing.JScrollPane();
-        SupplierCatalogTable = new javax.swing.JTable();
+        SalesOrderTable = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        txtActualPrice = new javax.swing.JTextField();
+        txtTargetPrice = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(0, 153, 153));
-        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         Back.setBackground(new java.awt.Color(255, 255, 255));
         Back.setForeground(new java.awt.Color(0, 0, 0));
@@ -82,9 +86,8 @@ public class ManageSalesPersonOrders extends javax.swing.JPanel {
                 BackActionPerformed(evt);
             }
         });
-        add(Back, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 350, -1, -1));
 
-        SupplierCatalogTable.setModel(new javax.swing.table.DefaultTableModel(
+        SalesOrderTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -100,26 +103,90 @@ public class ManageSalesPersonOrders extends javax.swing.JPanel {
                 return canEdit [columnIndex];
             }
         });
-        SupplierCatalogTable.addMouseListener(new java.awt.event.MouseAdapter() {
+        SalesOrderTable.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
-                SupplierCatalogTableMouseEntered(evt);
+                SalesOrderTableMouseEntered(evt);
             }
             public void mousePressed(java.awt.event.MouseEvent evt) {
-                SupplierCatalogTableMousePressed(evt);
+                SalesOrderTableMousePressed(evt);
             }
         });
-        jScrollPane1.setViewportView(SupplierCatalogTable);
+        jScrollPane1.setViewportView(SalesOrderTable);
 
         jScrollPane2.setViewportView(jScrollPane1);
 
-        add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 123, 560, 220));
-
         jLabel2.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
         jLabel2.setText("Browse Orders");
-        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, 550, 60));
 
         jLabel8.setText("Orders");
-        add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, -1, 20));
+
+        jLabel1.setText("Pricing information");
+
+        jLabel3.setText("Actual Price:");
+
+        jLabel4.setText("Target Price:");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 550, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addComponent(jLabel8)
+                .addGap(480, 480, 480)
+                .addComponent(jLabel1))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 510, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(50, 50, 50)
+                        .addComponent(jLabel4)
+                        .addGap(17, 17, 17))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(txtActualPrice, javax.swing.GroupLayout.DEFAULT_SIZE, 166, Short.MAX_VALUE)
+                    .addComponent(txtTargetPrice)))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(Back))
+        );
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jLabel3, jLabel4});
+
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(3, 3, 3)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(37, 37, 37)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtActualPrice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(txtTargetPrice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(7, 7, 7)
+                .addComponent(Back))
+        );
+
+        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jLabel3, jLabel4});
+
     }// </editor-fold>//GEN-END:initComponents
 
     private void BackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BackActionPerformed
@@ -129,22 +196,40 @@ public class ManageSalesPersonOrders extends javax.swing.JPanel {
 
     }//GEN-LAST:event_BackActionPerformed
 
-    private void SupplierCatalogTableMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SupplierCatalogTableMousePressed
+    private void SalesOrderTableMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SalesOrderTableMousePressed
         // TODO add your handling code 
-    }//GEN-LAST:event_SupplierCatalogTableMousePressed
+        int salestablesize = SalesOrderTable.getRowCount();
+        int selectedrow = SalesOrderTable.getSelectionModel().getLeadSelectionIndex();
 
-    private void SupplierCatalogTableMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SupplierCatalogTableMouseEntered
+        if (selectedrow < 0 || selectedrow > salestablesize - 1) {
+            return;
+        }
+        SolutionOrder solutionorder = (SolutionOrder) SalesOrderTable.getValueAt(selectedrow, 0);
+        if (solutionorder == null) {
+            return;
+        }
+
+        txtActualPrice.setText(String.valueOf(solutionorder.getActualPrice()));
+        txtTargetPrice.setText(String.valueOf(solutionorder.getSelectedsolutionoffer().getTargetPrice()));
+    }//GEN-LAST:event_SalesOrderTableMousePressed
+
+    private void SalesOrderTableMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SalesOrderTableMouseEntered
         // TODO add your handling code here:
-    }//GEN-LAST:event_SupplierCatalogTableMouseEntered
+    }//GEN-LAST:event_SalesOrderTableMouseEntered
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Back;
-    private javax.swing.JTable SupplierCatalogTable;
+    private javax.swing.JTable SalesOrderTable;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextField txtActualPrice;
+    private javax.swing.JTextField txtTargetPrice;
     // End of variables declaration//GEN-END:variables
 
 }
